4c6a82f1da86c0a7213f9e86107c7dfa5f1f27b0
obike <obikevictor66@gmail.com>
2025-02-04 15:27:07 -0800

    number counter Update

--- hand.js
+++ hand.js
@@ -11,3 +11,47 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
 }))
 
+let nums = document.querySelectorAll(".num1");
+let container = document.querySelector(".container");
+
+let test = false; // when the function doesn't start
+
+
+
+
+//// Counter
+const createOdometer = (el, value) => {
+  const odometer = new Odometer({
+    el: el,
+    value: 0,
+  });
+
+  let hasRun = false;
+
+  const options = {
+    threshold: [0, 0.9],
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (!hasRun) {
+          odometer.update(value);
+          hasRun = true;
+        }
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  observer.observe(el);
+};
+
+const subscribersOdometer = document.querySelector(".subscribers-odometer");
+createOdometer(subscribersOdometer, 8000);
+
+// const videosOdometer = document.querySelector(".videos-odometer");
+// createOdometer(videosOdometer, 790);
+
+const projectsOdometer = document.querySelector(".projects-odometer");
+createOdometer(projectsOdometer, 89);
